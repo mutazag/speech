2791b62016ffe9b916707681b68c8e7f1682ba2c
Mutaz Abu Ghazaleh <mutazag@hotmail.com>
2025-03-17 08:58:24 +1100

    Add endpoint configuration to Azure Speech Service sample

--- text-to-speech/config_sample.js
+++ text-to-speech/config_sample.js
@@ -1,6 +1,7 @@
 // Azure Speech Service Configuration
 const config = {
     speechRegion: "azure region",
+    endpoint: "https://<<your speech/cognitive service>>.cognitiveservices.azure.com/",
     speechKey: "speech key",
     speakerProfileId: "pretrained speaker profile id"
 };
